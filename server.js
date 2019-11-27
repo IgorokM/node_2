34cf20300030fba8a3bb2c2563e23e285375cefa
igorokiscool <igorokiscool@gmail.com>
2019-11-27 09:43:07 +0300

    added rout for analytic and refactoring

--- server.js
+++ server.js
@@ -1,27 +1,86 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const util = require('util');
 const app = express();
-
 const port = 8580;
 const pathToStatic = `${__dirname}/view`;
+const dbConnect = { user: 'node', password: '1234', database: 'node_2' };
+
 
 app.use(bodyParser.json());
-
 app.use(express.static(pathToStatic));
 
-function Question(req, res) {
-    
+
+function Db(config) {
+    const connection = mysql.createConnection(config);
+    return {
+        query(sql, args) {
+            return util.promisify(connection.query)
+                .call(connection, sql, args);
+        },
+        close() {
+            return util.promisify(connection.end).call(connection);
+        },
+        beginTransaction() {
+            return util.promisify(connection.beginTransaction)
+                .call(connection);
+        },
+        commit() {
+            return util.promisify(connection.commit)
+                .call(connection);
+        },
+        rollback() {
+            return util.promisify(connection.rollback)
+                .call(connection);
+        }
+    };
 }
 
-function Analytic(req, res) {
-    
+// function Question(req, res) {
+
+// }
+
+async function Analytic(req, res) {
+    let db = null;
+    try {
+        db = Db(dbConnect);
+        await db.beginTransaction();
+        res.send(await db.query(`SELECT name, count FROM answer`));
+    } catch (e) {
+        db.rollback();
+        res.status(400).send({ error: e.message, code: e.code });
+    }finally{
+        db.close();
+    }
+    db = null;
 }
 
-function Answer(req, res) {
-    res.send(req.body.answer);
+async function Answer(req, res) {
+    let db = null;
+    try {
+        db = Db(dbConnect);
+        await db.beginTransaction();
+        const answer = req.body.answer;
+        const data = await db.query(`SELECT count FROM answer WHERE name = ${answer}`);
+        const count = ++data[0].count;
+        const resuilt = await db.query(`UPDATE answer SET count = ${count} WHERE name = ${answer}`);
+        db.commit();
+        if (resuilt.changedRows) {
+            res.send({ name: answer, result: true });
+        } else {
+            res.send({ name: answer, result: false });
+        }
+    } catch (e) {
+        db.rollback();
+        res.status(400).send({ error: e.message, code: e.code });
+    } finally {
+        db.close();
+    }
+    db = null;
 }
 
-app.get('/variants', Question);
+//app.get('/variants', Question);
 app.post('/stat', Analytic);
 app.post('/vote', Answer);
 app.listen(port);
--- view/main.js
+++ view/main.js
@@ -1,16 +1,31 @@
 const containerEvents = document.getElementById('container-event');
 
-
-
-if (containerEvents) {
-    containerEvents.addEventListener('click', hendler);
-}
+if (containerEvents) containerEvents.addEventListener('click', hendler);
 
 async function hendler(event) {
     const target = event.target;
     if (target && target.tagName === 'BUTTON') {
-        const params = { headers: { 'Content-Type': 'application/json' }, method: 'POST', body: JSON.stringify({ answer: target.id }) };
-        const data = await fetch('/vote', params);
-        console.log(await data.json());
+        const params = { headers: { 'Content-Type': 'application/json' }, method: 'POST', body: null };
+        try {
+            if (target.id === '1' || target.id === '2') {
+                params.body = JSON.stringify({ answer: target.id });
+                let data = await fetch('/vote', params);
+                data = await data.json();
+                if (data.hasOwnProperty('result') && data.result) alert('Ваш голос принят');
+            } else if (target.id === 'stat') {
+                let data = await fetch('/stat', params);
+                data = await data.json();
+                if (Array.isArray(data)) {
+                    for (let obj of data) {
+                        let progress = document.getElementById(obj.name);
+                        if (progress) {
+                            progress.value = obj.count;
+                        }
+                    }
+                }
+            }
+        } catch (e) {
+            console.error(e.message);
+        }
     }
 }
